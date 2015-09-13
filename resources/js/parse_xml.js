dca43787881d7b431c2f7e42e38d0853275893ea
Matteo Bernardini <mat_teobe@yahoo.it>
2015-09-13 12:54:55 +0200

    Fixed tabs, removed underscores from private functions

--- resources/js/parse_xml.js
+++ resources/js/parse_xml.js
@@ -9,50 +9,50 @@ function XMLParser() {
 
 	// == PRIVATE == //
 
-	var _req = new XMLHttpRequest(), n, q, callback;
+	var req = new XMLHttpRequest(), n, q, callback;
 
 //TODO: define this var somewhere else. It stands for default "Answers Per Question"
 	var APQ = 4;
 
 
 	// Will request the XML file and parse it to a DOMObject
-	function _getXML(url) {
-		_req.open("GET", url);
-		_req.send();
-		_req.onreadystatechange = function() {
-			if (_req.readyState == 4) {
-				if (_req.status == 200)
-					_parseLevel(200, _req.responseXML);
+	function getXML(url) {
+		req.open("GET", url);
+		req.send();
+		req.onreadystatechange = function() {
+			if (req.readyState == 4) {
+				if (req.status == 200)
+					parseLevel(200, req.responseXML);
 				else
-					_parseLevel(_req.status);
+					parseLevel(req.status);
 			}
 		};
-		
+
 	}
-	
-	// Will actually retrieve level data	
-	function _parseLevel(status, xml) {
+
+	// Will actually retrieve level data
+	function parseLevel(status, xml) {
 		if (status==200) {
 
 			var qs		=  xml.getElementsByTagName("question"),
 			    data	=  [],	// output var
 			    pick_q	=  [],	// picked questions (id)
 			    count_q	=  qs.length;
-			    
+
 			while (pick_q.length < q && pick_q.length < count_q) {
 				var r1 = parseInt(Math.random()*count_q);
 				// If this question has already been chosen, retry
 				if (~pick_q.indexOf(r1)) continue;
 				pick_q.push(r1);
-				
+
 				var snip	=  qs[r1].getElementsByTagName("snippet")[0].textContent.trim(),
 				    ans_xml	=  qs[r1].getElementsByTagName("answers")[0],
 				    ans		=  [],	// output answers
 				    pick_a	=  [],	// picked answers (id)
 				    count_a	=  ans_xml.children.length-1,
-			        id_com	=  [],	// id of answers which are comments
-			        id_r;			// id of the right answer
-				    
+				    id_com	=  [],	// id of answers which are comments
+				    id_r;			// id of the right answer
+
 				// Take the right answer
 				var right =	ans_xml.getElementsByTagName("rightans")[0] ||
 							ans_xml.getElementsByTagName("rightcom")[0];
@@ -61,7 +61,7 @@ function XMLParser() {
 					["right", right.tagName.slice(5)]
 				]);
 				ans_xml.removeChild(right);
-				
+
 				// Choose randomly other answers/comments
 				while (ans.length < APQ && ans.length < count_a+1) {
 					var r2 = parseInt(Math.random()*count_a);
@@ -72,12 +72,12 @@ function XMLParser() {
 						[ ans_xml.children[r2].tagName ]
 					])
 				}
-				
+
 				// Shuffle answers (this alghoritm is pretty funny! found on csstricks)
 				(function(o){
 				for(var j,x,i=o.length;i;j=parseInt(Math.random()*i),x=o[--i],o[i]=o[j],o[j]=x);
 				})(ans);
-				
+
 				// Remove labels and position "other" to the end
 				for (var i=0; i < ans.length; i++) {
 					if (ans[i][0].toLowerCase() == "other" && i!=ans.length-1)
@@ -88,7 +88,7 @@ function XMLParser() {
 						id_com.push(i);
 					ans[i] = ans[i][0];
 				}
-				
+
 				// Prepare output
 				data.push({
 					"snippet":		snip,
@@ -98,9 +98,9 @@ function XMLParser() {
 				});
 
 			}
-			
+
 			callback({"status": "ok", "data": data});
-			
+
 		}
 		else if (status == 404)
 			callback({"status": "error", "type": "LEVEL_NOT_EXIST"});
@@ -120,7 +120,7 @@ function XMLParser() {
 		n = _n;
 		q = _q || APQ;
 		callback = _callback;
-		_getXML("levels/xml/"+n+".xml");
+		getXML("levels/xml/"+n+".xml");
 	}
 
 }
