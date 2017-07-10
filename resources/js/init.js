9a7c53b008901a1c3182d2a3150d43dafdebeaaf
Matteo Bernardini <mat_teobe@yahoo.it>
2017-07-10 22:19:24 +0200

    Correctly applied Apache 2.0 license to sources.

--- resources/js/init.js
+++ resources/js/init.js
@@ -1,3 +1,20 @@
+/*
+ * (c) Marco Bonelli 2015
+ * (c) Matteo Bernardini 2015
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 function main() {
 
 	// Try to resume the game
@@ -75,7 +92,7 @@ if (MOBILE) {
 	// Prevent inappropriate scrolling on iOS (simulate app)
 	document.addEventListener("touchmove", function(e){e.preventDefault();});
 
-	// Handle PressEvent → TouchEvent	
+	// Handle PressEvent → TouchEvent
 	var tapping = false;
 	document.addEventListener("touchstart", function() { tapping = true; }, true);
 	document.addEventListener("touchmove", function() { tapping = false; }, true);
